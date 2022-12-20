4f43b0f5f164750c732f2d284b9a6f6fce2cd9f2
buray <18810602951@163.com>
2022-12-20 20:19:13 +0800

    test

---
+++ StudyC++/main.cpp
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+int main(int argc, char const *argv[])
+{
+    cout << "123" << endl;
+    return 0;
+}
